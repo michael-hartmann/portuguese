9f7e68f185c168a0ddcfe8b0054c3d8a649f17e6
Michael Hartmann <michael.hartmann@physik.uni-augsburg.de>
2016-11-06 23:31:32 +0100

    initial commit

---
+++ voc.py
@@ -0,0 +1,30 @@
+from sys import argv
+from random import shuffle
+
+def slurp(filename):
+    data = []
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.strip()
+            if len(line) == 0 or line[0] == "#":
+                continue
+            left, right = line.split(";")
+            data.append((left.strip(), right.strip()))
+    return data
+
+filename = argv[1]
+data = slurp(filename)
+
+shuffle(data)
+
+total = len(data)
+correct = 0
+for i,(left,right) in enumerate(data):
+    entry = input("[%d/%d] " % (i+1,total) + left + ": ")
+    entry = entry.strip()
+    if entry != right:
+        print("Falsch: Eingabe \"%s\", korrekt \"%s\"" % (entry, right))
+    else:
+        correct += 1
+
+print("%d/%d (%.2f%%)" % (correct, total, correct/total*100))
